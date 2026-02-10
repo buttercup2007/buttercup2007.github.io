135d5a7654acfab5a086764d4f94b1492d15c936
buttercup2007 <2215907@talnet.nl>
2026-02-10 10:44:13 +0100

    start van to do list

--- testproject/routes/web.php
+++ testproject/routes/web.php
@@ -1,5 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>to do list</title>
+</head>
+<body>
+    <h1>to do list</h1>
+    <style>body { background-color: #51ff00; }</style>
+</body>
+</html>
+
 <?php
 
 Route::get('/', function () {
-    return 'Hello world';
+    
 });
+
