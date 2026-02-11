1b813d0417c9c8937105529c1a5455e99df05105
buttercup2007 <2215907@talnet.nl>
2026-02-11 10:55:47 +0100

    vue succsesgully connected

--- testproject/vite.config.js
+++ testproject/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css'],
+            input: ['resources/js/app.js', 'resources/css/app.css'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(), // <-- this is required
     ],
-    
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
