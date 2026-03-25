c92dec847b555e3ab7df8ba07449e18a36d2cced
buttercup2007 <2215907@talnet.nl>
2026-03-25 13:17:20 +0100

    js added

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const sections = document.querySelectorAll(".scroll-section");
+
+    function checkVisibility () {
+         const triggerBottom = window.innerHeight * 0.85;
+
+         sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+
+            if(section < triggerBottom) {
+                section.classList.add("visible");
+            }
+         });
+    }
+
+    window.addEventListener("scroll", checkVisibility);
+    checkVisibility();
+})
