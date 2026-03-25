5889eb458441f088772e04351abfc684277093a2
buttercup2007 <2215907@talnet.nl>
2026-03-25 13:29:22 +0100

    its scrollable now

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sections = document.querySelectorAll(".scroll-section");
 
-    function checkVisibility () {
+    function checkVisibility() {
          const triggerBottom = window.innerHeight * 0.85;
 
          sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
-
             if(section < triggerBottom) {
                 section.classList.add("visible");
             }
